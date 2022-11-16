0ff9872be9fc8e118eeb5bf79cbb31cff71992df
jtrongkhoa93 <43825182+jtrongkhoa93@users.noreply.github.com>
2022-11-16 20:39:48 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,423 @@
+import functools
+from pathlib import Path
+
+import streamlit as st
+from st_aggrid import AgGrid
+from st_aggrid.shared import JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+import pandas as pd
+import plotly.express as px
+from typing import List
+
+import numpy as np
+import math
+import re
+import pandas
+from sklearn import model_selection
+from scipy.sparse import csr_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+
+chart = functools.partial(st.plotly_chart, use_container_width=True)
+COMMON_ARGS = {
+    "color": "symbol",
+    "color_discrete_sequence": px.colors.sequential.Greens,
+    "hover_data": [
+        "account_name",
+        "percent_of_account",
+        "quantity",
+        "total_gain_loss_dollar",
+        "total_gain_loss_percent",
+    ],
+}
+
+
+class MF(object):
+    """docstring for CF"""
+    def __init__(self, Y_data, K, lam = 0.1, Xinit = None, Winit = None, 
+            learning_rate = 0.5, max_iter = 1000, print_every = 100, user_based = 1):
+        self.Y_raw_data = Y_data
+        self.K = K
+        # regularization parameter
+        self.lam = lam
+        # learning rate for gradient descent
+        self.learning_rate = learning_rate
+        # maximum number of iterations
+        self.max_iter = max_iter
+        # print results after print_every iterations
+        self.print_every = print_every
+        # user-based or item-based
+        self.user_based = user_based
+        # number of users, items, and ratings. Remember to add 1 since id starts from 0
+        self.n_users = int(np.max(Y_data[:, 0])) + 1 
+        self.n_items = int(np.max(Y_data[:, 1])) + 1
+        self.n_ratings = Y_data.shape[0]
+        
+        if Xinit is None: # new
+            self.X = np.random.randn(self.n_items, K)
+        else: # or from saved data
+            self.X = Xinit 
+        
+        if Winit is None: 
+            self.W = np.random.randn(K, self.n_users)
+        else: # from saved data
+            self.W = Winit
+            
+        # normalized data, update later in normalized_Y function
+        self.Y_data_n = self.Y_raw_data.copy()
+
+
+    def normalize_Y(self):
+        if self.user_based:
+            user_col = 0
+            item_col = 1
+            n_objects = self.n_users
+
+        # if we want to normalize based on item, just switch first two columns of data
+        else: # item bas
+            user_col = 1
+            item_col = 0 
+            n_objects = self.n_items
+
+        users = self.Y_raw_data[:, user_col] 
+        self.mu = np.zeros((n_objects,))
+        for n in range(n_objects):
+            # row indices of rating done by user n
+            # since indices need to be integers, we need to convert
+            ids = np.where(users == n)[0].astype(np.int32)
+            # indices of all ratings associated with user n
+            item_ids = self.Y_data_n[ids, item_col] 
+            # and the corresponding ratings 
+            ratings = self.Y_data_n[ids, 2]
+            # take mean
+            m = np.mean(ratings) 
+            if np.isnan(m):
+                m = 0 # to avoid empty array and nan value
+            self.mu[n] = m
+            # normalize
+            self.Y_data_n[ids, 2] = ratings - self.mu[n]
+
+    """
+    Khi có dữ liệu mới, cập nhận Utility matrix bằng cách thêm các hàng này vào cuối Utility Matrix. Để cho đơn giản, giả sử rằng không có users hay items mới, cũng không có ratings nào bị thay đổi.
+    """
+    def add(self, new_data):
+        """
+        Update Y_data matrix when new ratings come.
+        For simplicity, suppose that there is no new user or item.
+        """
+        self.Y_raw_data = np.concatenate((self.Y_raw_data, new_data), axis = 0)
+        self.Y_data_n = self.Y_raw_data.copy()
+        self.n_users = int(np.max(self.Y_data[:, 0])) + 1 
+        self.n_items = int(np.max(self.Y_data[:, 1])) + 1
+        self.normalize_Y()
+
+
+    # Tính giá trị hàm mất mát:
+    def loss(self):
+        L = 0 
+        for i in range(self.n_ratings):
+            # user, item, rating
+            n, m, rate = int(self.Y_data_n[i, 0]), int(self.Y_data_n[i, 1]), self.Y_data_n[i, 2]
+            L += 0.5*(rate - self.X[m, :].dot(self.W[:, n]))**2
+        
+        # take average
+        L /= self.n_ratings
+        # regularization, don't ever forget this 
+        L += 0.5*self.lam*(np.linalg.norm(self.X, 'fro') + np.linalg.norm(self.W, 'fro'))
+        return L
+
+
+    def updateX(self):
+        for m in range(self.n_items):
+            user_ids, ratings = self.get_users_who_rate_item(m)
+            Wm = self.W[:, user_ids]
+            # gradient
+            grad_xm = -(ratings - self.X[m, :].dot(Wm)).dot(Wm.T)/self.n_ratings + \
+                                               self.lam*self.X[m, :]
+            self.X[m, :] -= self.learning_rate*grad_xm.reshape((self.K,))
+    
+    def updateW(self):
+        for n in range(self.n_users):
+            item_ids, ratings = self.get_items_rated_by_user(n)
+            Xn = self.X[item_ids, :]
+            # gradient
+            grad_wn = -Xn.T.dot(ratings - Xn.dot(self.W[:, n]))/self.n_ratings + \
+                        self.lam*self.W[:, n]
+            self.W[:, n] -= self.learning_rate*grad_wn.reshape((self.K,))
+
+    
+    # Xác định các items được đánh giá bởi 1 user, và users đã đánh giá 1 item và các ratings tương ứng:
+    def get_items_rated_by_user(model, user_id):
+        """
+        get all items which are rated by user user_id, and the corresponding ratings
+        """
+        ids = np.where(model.Y_data_n[:,0] == user_id)[0] 
+        item_ids = model.Y_data_n[ids, 1].astype(np.int32) # indices need to be integers
+        ratings = model.Y_data_n[ids, 2]
+        return (item_ids, ratings)
+        
+        
+    def get_users_who_rate_item(model, item_id):
+        """
+        get all users who rated item item_id and get the corresponding ratings
+        """
+        ids = np.where(model.Y_data_n[:,1] == item_id)[0] 
+        user_ids = model.Y_data_n[ids, 0].astype(np.int32)
+        ratings = model.Y_data_n[ids, 2]
+        return (user_ids, ratings)
+
+    def fit(self):
+        self.normalize_Y()
+        for it in range(self.max_iter):
+            self.updateX()
+            self.updateW()
+            if (it + 1) % self.print_every == 0:
+                rmse_train = self.evaluate_RMSE(self.Y_raw_data)
+                print ('iter =', it + 1, ', loss =', self.loss(), ', RMSE train =', rmse_train)
+
+    def pred(self, u, i):
+        """ 
+        predict the rating of user u for item i 
+        if you need the un
+        """
+        u = int(u)
+        i = int(i)
+        if self.user_based:
+            bias = self.mu[u]
+        else: 
+            bias = self.mu[i]
+        pred = self.X[i, :].dot(self.W[:, u]) + bias 
+        # truncate if results are out of range [0, 5]
+        if pred < 0:
+            return 0 
+        if pred > 5: 
+            return 5 
+        return pred 
+        
+    
+    def pred_for_user(self, user_id):
+        """
+        predict ratings one user give all unrated items
+        """
+        ids = np.where(self.Y_data_n[:, 0] == user_id)[0]
+        items_rated_by_u = self.Y_data_n[ids, 1].tolist()              
+        
+        y_pred = self.X.dot(self.W[:, user_id]) + self.mu[user_id]
+        predicted_ratings= []
+        for i in range(self.n_items):
+            if i not in items_rated_by_u:
+                predicted_ratings.append((i, y_pred[i]))
+        
+        return predicted_ratings
+
+    def evaluate_RMSE(self, rate_test):
+        n_tests = rate_test.shape[0]
+        SE = 0 # squared error
+        for n in range(n_tests):
+            pred = self.pred(rate_test[n, 0], rate_test[n, 1])
+            SE += (pred - rate_test[n, 2])**2 
+
+        RMSE = np.sqrt(SE/n_tests)
+        return RMSE
+
+# save the model to disk
+filename = 'finalized_model_2.sav'
+loaded_rs = pickle.load(open(filename, 'rb'))
+
+loaded_rs.Y_data_n
+
+
+@st.experimental_memo
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Take Raw Fidelity Dataframe and return usable dataframe.
+    - snake_case headers
+    - Include 401k by filling na type
+    - Drop Cash accounts and misc text
+    - Clean $ and % signs from values and convert to floats
+
+    Args:
+        df (pd.DataFrame): Raw fidelity csv data
+
+    Returns:
+        pd.DataFrame: cleaned dataframe with features above
+    """
+    df = df.copy()
+    df.columns = df.columns.str.lower().str.replace(" ", "_", regex=False).str.replace("/", "_", regex=False)
+
+    df.type = df.type.fillna("unknown")
+    df = df.dropna()
+
+    price_index = df.columns.get_loc("last_price")
+    cost_basis_index = df.columns.get_loc("cost_basis_per_share")
+    df[df.columns[price_index : cost_basis_index + 1]] = df[
+        df.columns[price_index : cost_basis_index + 1]
+    ].transform(lambda s: s.str.replace("$", "", regex=False).str.replace("%", "", regex=False).astype(float))
+
+    quantity_index = df.columns.get_loc("quantity")
+    most_relevant_columns = df.columns[quantity_index : cost_basis_index + 1]
+    first_columns = df.columns[0:quantity_index]
+    last_columns = df.columns[cost_basis_index + 1 :]
+    df = df[[*most_relevant_columns, *first_columns, *last_columns]]
+    return df
+
+
+@st.experimental_memo
+def filter_data(df: pd.DataFrame, account_selections: List[str], symbol_selections: List[str]) -> pd.DataFrame:
+    """
+    Returns Dataframe with only accounts and symbols selected
+
+    Args:
+        df (pd.DataFrame): clean fidelity csv data, including account_name and symbol columns
+        account_selections (list[str]): list of account names to include
+        symbol_selections (list[str]): list of symbols to include
+
+    Returns:
+        pd.DataFrame: data only for the given accounts and symbols
+    """
+    df = df.copy()
+    df = df[
+        df.account_name.isin(account_selections) & df.symbol.isin(symbol_selections)
+    ]
+
+    return df
+
+
+def main() -> None:
+    st.header("Fidelity Account Overview :moneybag: :dollar: :bar_chart:")
+
+    # with st.expander("How to Use This"):
+    #     st.write(Path("README.md").read_text())
+
+    st.subheader("Upload your CSV from Fidelity")
+
+    df = pd.read_csv(uploaded_data)
+    with st.expander("Raw Dataframe"):
+        st.write(df)
+
+    df = clean_data(df)
+    with st.expander("Cleaned Data"):
+        st.write(df)
+
+    st.sidebar.subheader("Filter Displayed Accounts")
+
+    accounts = list(df.account_name.unique())
+    account_selections = st.sidebar.multiselect(
+        "Select Accounts to View", options=accounts, default=accounts
+    )
+    st.sidebar.subheader("Filter Displayed Tickers")
+
+    symbols = list(df.loc[df.account_name.isin(account_selections), "symbol"].unique())
+    symbol_selections = st.sidebar.multiselect(
+        "Select Ticker Symbols to View", options=symbols, default=symbols
+    )
+
+    age = st.slider('How old are you?', 0, 5, 5)
+    st.write("I'm ", age, 'years old')
+
+    df = filter_data(df, account_selections, symbol_selections)
+    st.subheader("Selected Account and Ticker Data")
+    cellsytle_jscode = JsCode(
+        """
+    function(params) {
+        if (params.value > 0) {
+            return {
+                'color': 'white',
+                'backgroundColor': 'forestgreen'
+            }
+        } else if (params.value < 0) {
+            return {
+                'color': 'white',
+                'backgroundColor': 'crimson'
+            }
+        } else {
+            return {
+                'color': 'white',
+                'backgroundColor': 'slategray'
+            }
+        }
+    };
+    """
+    )
+
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_columns(
+        (
+            "last_price_change",
+            "total_gain_loss_dollar",
+            "total_gain_loss_percent",
+            "today's_gain_loss_dollar",
+            "today's_gain_loss_percent",
+        ),
+        cellStyle=cellsytle_jscode,
+    )
+    gb.configure_pagination()
+    gb.configure_columns(("account_name", "symbol"), pinned=True)
+    gridOptions = gb.build()
+
+    AgGrid(df, gridOptions=gridOptions, allow_unsafe_jscode=True)
+
+    def draw_bar(y_val: str) -> None:
+        fig = px.bar(df, y=y_val, x="symbol", **COMMON_ARGS)
+        fig.update_layout(barmode="stack", xaxis={"categoryorder": "total descending"})
+        chart(fig)
+
+    account_plural = "s" if len(account_selections) > 1 else ""
+    st.subheader(f"Value of Account{account_plural}")
+    totals = df.groupby("account_name", as_index=False).sum()
+    if len(account_selections) > 1:
+        st.metric(
+            "Total of All Accounts",
+            f"${totals.current_value.sum():.2f}",
+            f"{totals.total_gain_loss_dollar.sum():.2f}",
+        )
+    for column, row in zip(st.columns(len(totals)), totals.itertuples()):
+        column.metric(
+            row.account_name,
+            f"${row.current_value:.2f}",
+            f"{row.total_gain_loss_dollar:.2f}",
+        )
+
+    fig = px.bar(
+        totals,
+        y="account_name",
+        x="current_value",
+        color="account_name",
+        color_discrete_sequence=px.colors.sequential.Greens,
+    )
+    fig.update_layout(barmode="stack", xaxis={"categoryorder": "total descending"})
+    chart(fig)
+
+    st.subheader("Value of each Symbol")
+    draw_bar("current_value")
+
+    st.subheader("Value of each Symbol per Account")
+    fig = px.sunburst(
+        df, path=["account_name", "symbol"], values="current_value", **COMMON_ARGS
+    )
+    fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+    chart(fig)
+
+    st.subheader("Value of each Symbol")
+    fig = px.pie(df, values="current_value", names="symbol", **COMMON_ARGS)
+    fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+    chart(fig)
+
+    st.subheader("Total Value gained each Symbol")
+    draw_bar("total_gain_loss_dollar")
+    st.subheader("Total Percent Value gained each Symbol")
+    draw_bar("total_gain_loss_percent")
+
+
+
+
+if __name__ == "__main__":
+    st.set_page_config(
+        "Fidelity Account View by Gerard Bentley",
+        "📊",
+        initial_sidebar_state="expanded",
+        layout="wide",
+    )
+    main()
